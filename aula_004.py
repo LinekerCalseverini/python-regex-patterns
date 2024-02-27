eac878754bc06960805afdd177d2280cc847f57c
LinekerCalseverini <lineker.calseverini@gmail.com>
2024-02-27 13:05:07 -0300

    Greedy e non-greedy (lazy)

---
+++ aula_004.py
@@ -0,0 +1,12 @@
+# Meta caracteres: ^ $ * + ? { } \ ( )
+# * 0 ou n
+# + 1 ou n {1,}
+# ? 0 ou 1
+import re
+
+texto: str = (
+    '<p>Frase 1</p> <p>Frase 2</p> <p>Frase 3</p> <div>Frase 4</div>'
+)
+
+print(re.findall(r'<[pdiv]{1,3}>(?P<frase>.*)</[pdiv]{1,3}>', texto))
+print(re.findall(r'<[pdiv]{1,3}>(?P<frase>.*?)</[pdiv]{1,3}>', texto))
